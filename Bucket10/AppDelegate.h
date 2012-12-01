4a978f85b15b75c5394a81cb5cab9febf9e0d123
Brad Griffith <braddgriffith@gmail.com>
2012-12-01 13:07:46 -0800

    Initial Commit

---
+++ Bucket10/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bucket10
+//
+//  Created by Brad Grifffith on 12/1/12.
+//  Copyright (c) 2012 Brad Grifffith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
